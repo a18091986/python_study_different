6b9458f36023d2573220347ea72d311eda038931
behappyman <behappyman@mail.ru>
2022-01-18 14:39:19 +0300

    clearing

---
+++ TG_BOT_save_queseetion/SQL/SQL_bot.sql
@@ -0,0 +1,17 @@
+﻿create database if not exists QBot;
+
+use QBot;
+
+drop table if exists Question;
+
+create table if not exists Question(
+  id int auto_increment not null,
+  question varchar(10000),
+  links varchar(200),
+  description varchar(1000),
+  primary key (id)
+  );
+
+-- insert into Question (question) VALUE ('dfgdf');
+
+select * from Question;
---
+++ TG_BOT_save_queseetion/db_connect_.py
@@ -0,0 +1,16 @@
+import mysql.connector as mc
+
+def connect_to_db():
+    try:
+        mydb = mc.connect(
+        host = '192.168.2.165',
+        user = 'admin',
+        password = '0gfyfctyr0MYSQL',
+        database = 'QBot')
+
+        print('Connected')
+        return mydb
+    except mc.Error as e:
+        print(e)
+
+# connect_to_db()
