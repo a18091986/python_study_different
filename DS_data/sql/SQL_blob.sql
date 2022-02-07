066fc523beaaa3848a85a70a83042cd7c0ea5439
behappyman <behappyman@mail.ru>
2022-02-08 00:00:53 +0300

    *

---
+++ DS_data/sql/SQL_blob.sql
@@ -0,0 +1,17 @@
+﻿create database if not exists image;
+
+use image;
+
+drop table if exists imag;
+
+create table if not exists imag(
+
+  id tinyint unsigned auto_increment,
+  name varchar(100) not null,
+  img longblob not null,
+  primary key(id));
+
+select * from imag;
+
+паопароn
+insert into imag (name, img) values ('123', '123das');
