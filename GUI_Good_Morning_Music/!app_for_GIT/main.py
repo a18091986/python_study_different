98740131a207e151bfbdc438e08f02172de54782
behappyman <behappyman@mail.ru>
2022-04-14 00:37:36 +0300

    clear

--- GUI_Good_Morning_Music/!app_for_GIT/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from music_management_system import Music_management_system
-
-
-app = QApplication(sys.argv)
-Music_management_system = Music_management_system()
-sys.exit(app.exec())
